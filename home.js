b58621eb13814b17a307153f533e5a6dae38f68e
SudhirDeshwal <50604228+SudhirDeshwal@users.noreply.github.com>
2020-05-24 06:48:05 -0400

    Mini Project 1 commit

---
+++ home.js
@@ -0,0 +1,185 @@
+// // //alert("ram");
+// // //document.getElementById("demo").innerHTML="ram";
+
+// // //var age =prompt('whats your age');
+// // //document.getElementById("demo").innerHTML=age;
+// // num1 = 10;
+// // num1 += 10;
+// // console.log(num1);
+// // //functions and call it
+
+// // // function fun()
+// // // {
+// // //     console.log('this is a function');
+// // // }
+// // // //call
+// // // fun();
+
+// // function greeting_here()
+// // {
+// //     var name = prompt('whats your name');
+// //     var result = 'Heloo'+  "  " +name;
+// //     console.log(result)
+// // }
+// // //greeting_here();
+
+// // // function with arguments
+
+// // function SumNumbers(a , b)
+// // {
+// //      var result = a + b
+// //      console.log(result)
+// // }
+// // //SumNumbers(10,10);
+// // // function promt.
+// // function greeting(yourname)
+// // {
+// //     var result = 'Heloo'+  "  " + yourname;
+// //     console.log(result)
+// // }
+
+// // //var namehere = prompt("whats your name");
+// // //greeting(namehere);
+// // // while loops
+// // //  var num = 0;
+// // // while(num < 10) {
+
+// // //     num = num+1;
+// // //     console.log(num);
+// // // }
+// // // for loops
+
+// // for(let num = 0 ; num <= 100 ; num++){
+// //     console.log(num);
+// // }
+// //data types
+// let yourage = 18;
+// let yourname = "sudhir"; //STRINGS
+// let name = {first : 'jane',last: "Doe"}; //object
+// let truth = false;
+// let grocerices = ["apple","bana",'oranges']; //array
+// let random; //undefined
+// let nothing = null; //value null
+
+// //Strings
+
+// let fruit = "banana";
+// let morefruits = 'apple\norange';   //new line
+// console.log(morefruits);
+// console.log(fruit.length);
+// console.log(fruit.indexOf("b"));
+// console.log(fruit.slice(2,6));
+// console.log(fruit.replace("b" ,"2"));
+// console.log(fruit.toLowerCase("b"));
+// console.log(fruit.toUpperCase(fruit));
+// console.log(fruit.charAt(2));
+// console.log(fruit[2]);
+// console.log(fruit.split(','));
+// console.log(fruit.split(''));
+
+// //Arrays
+// let fruits = ['apple',"orange","pinpealls"];
+// //let fruits1 = new Array('apple',"orange","pinpealls");
+// //alert(fruits[1]);
+// console.log(fruits);
+// console.log(fruits[3]);
+
+// fruits[0]='pear';
+// console.log(fruits);
+
+// for(let i = 0 ;i < fruits.length; i++){
+// console.log(fruits[i]);
+// }
+
+// console.log('to strig' ,fruits.toString());
+// console.log(fruits.join("*"));
+// console.log(fruits.pop(),fruits);
+// console.log(fruits.push("grapes"),fruits);
+// console.log(fruits[2]);
+// console.log(fruits[4]);
+// fruits[3] = "new fruits 1";
+// console.log(fruits[2]);
+// console.log(fruits);
+
+// let veg = ['tomates',"asapa","potaoes"];
+
+// let allgroc = fruits.concat(veg);
+// console.log(allgroc);
+
+//object/dictionary  in js
+
+
+// let student = {
+//     first:"ram",
+//     second:"karan",
+//     third:"shivam",
+//     studentinfo : function(){
+
+//         return this.first + '\n' + this.third;
+//     } 
+//             };
+// console.log(student.first);
+// console.log(student.studentinfo());
+
+
+// Conditionals and conftrols in js (if else)
+// var age = prompt("whats your age");
+// if((age>= 18) && (age<=35)){
+
+//     status = 'target demo';
+//     console.log(status);
+// }
+// else {
+
+//     status = 'not my audience';
+//     console.log(status);
+// }
+
+// switch in js
+// switch(5){
+
+// case 0 : text = "weekend";
+// break;
+// case 5 : text = "weekend";
+// break;
+// case 6 : text = "weekend";
+// break;
+// default : text = "weekday";
+// }
+// console.log(text);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
